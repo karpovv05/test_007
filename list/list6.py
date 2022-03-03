f7ec595c45feb0187b5742c2c680cea83ed1dff4
karpovv05 <karps5599@icloud.com>
2022-03-03 16:09:22 +0400

    1

--- list/list6.py
+++ list/list6.py
@@ -2,14 +2,13 @@ from math import *
 
 
 def fun(x):
-    za = cos(x*2) + sin(x-3)
+    za = x %10
     return za
+
+
 
 
 fa = fun(int(input("a= ")))
 fb = fun(int(input("b= ")))
+print(fa+fb)
 
-if fa > fb:
-    print('b=', fb)
-else:
-    print("a=", fa)
