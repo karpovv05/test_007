70b21df10370d64d841d7148179c0b18b122ba0c
karpovv05 <karps5599@icloud.com>
2022-03-03 16:12:46 +0400

    Create list7.py

---
+++ list/list7.py
@@ -0,0 +1,16 @@
+from math import *
+
+
+def fun(x):
+    za = x %100//10
+    return za
+
+
+
+
+fa = fun(int(input("a= ")))
+fb = fun(int(input("b= ")))
+fc = fun(int(input("c= ")))
+
+print(fa+fb-fc)
+
